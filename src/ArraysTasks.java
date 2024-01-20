1be6eeafe3498eadca81b39c37e0c0568bd36d15
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-20 12:46:51 +0200

    solved sumEvenNums && evenOddSubNums && equalArrs && printEachNth

--- src/ArraysTasks.java
+++ src/ArraysTasks.java
@@ -7,7 +7,116 @@ public class ArraysTasks {
 //        sumFirstAndLast();
 //        weekDay();
 //        reverseArr();
-        reverseStrArr();
+//        reverseStrArr();
+//        sumEvenNums();
+//        evenOddSubNums();
+//        equalArrs();
+//        condenseArr();
+        printEachNth();
+    }
+
+    private static void printEachNth() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("get arr");
+
+        String[] inputArr = sc.nextLine().split(",");
+
+        System.out.println("step");
+        int step = sc.nextInt();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < inputArr.length; i += step) {
+            result.append(inputArr[i]).append(" ");
+        }
+
+        result.append("\n");
+        System.out.println(result.toString());
+
+    }
+
+    private static void condenseArr() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("get arr");
+        int[] inputOne = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+
+        while (inputOne.length > 1) {
+            int[] sums = new int[inputOne.length - 1];
+
+            for (int i = 1; i < inputOne.length; i++) {
+                sums[i - 1] = inputOne[i - 1] + inputOne[i];
+            }
+
+            inputOne = sums;
+        }
+
+        System.out.println(inputOne[0]);
+    }
+
+    private static void equalArrs() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("get arr");
+        int[] inputOne = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+        System.out.println("get arr");
+        int[] inputTwo = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+
+        int sum = 0;
+
+        for (int i = 0; i < inputOne.length; i++) {
+            if (i == inputTwo.length) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.\n", i);
+                return;
+            }
+
+            if (inputTwo[i] != inputOne[i]) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.\n", i);
+                return;
+            }
+
+            if (i == inputOne.length - 1 && i < inputTwo.length - 1) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.\n", (i + 1));
+                return;
+            }
+
+            sum += inputOne[i];
+        }
+
+        System.out.printf("Arrays are identical. Sum: %d", sum);
+    }
+
+    private static void evenOddSubNums() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("get arr");
+        int evenSum = 0;
+        int oddSum = 0;
+        int[] input = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 == 0) {
+                evenSum += input[i];
+            } else {
+                oddSum += input[i];
+            }
+        }
+
+        System.out.println(evenSum - oddSum);
+    }
+
+    private static void sumEvenNums() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("get arr");
+
+        int[] input = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+
+        int result = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 == 0) {
+                result += input[i];
+            }
+        }
+
+        System.out.println(result);
 
     }
 
