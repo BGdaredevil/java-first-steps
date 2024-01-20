0cc12b09072796c6468064d747b9c84f200af6ea
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-20 11:27:45 +0200

    solved reverseArr && reverseStrArr

--- src/ArraysTasks.java
+++ src/ArraysTasks.java
@@ -6,6 +6,55 @@ public class ArraysTasks {
 
 //        sumFirstAndLast();
 //        weekDay();
+//        reverseArr();
+        reverseStrArr();
+
+    }
+
+    private static void reverseStrArr() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("get num");
+        String[] input = sc.nextLine().split(",");
+
+        for (int i = 0; i < input.length / 2; i++) {
+            String temp = input[i];
+            input[i] = input[input.length - 1 - i];
+            input[input.length - 1 - i] = temp;
+
+        }
+
+        for (var i : input) {
+            System.out.printf("%s ", i);
+        }
+
+    }
+
+    private static void reverseArr() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("get num");
+        int count = Integer.parseInt(sc.nextLine());
+        System.out.println("get arr");
+        String arrInputStr = sc.nextLine();
+
+        int[] input = Arrays.stream(arrInputStr.split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] result = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            if (input.length > i && result.length > i) {
+                result[i] = input[i];
+            }
+        }
+
+        for (int i = 0; i < count / 2; i++) {
+            result[i] ^= result[result.length - 1 - i];
+            result[result.length - 1 - i] ^= result[i];
+            result[i] ^= result[result.length - 1 - i];
+        }
+
+        for (int i : result) {
+            System.out.printf("%d ", i);
+        }
+
     }
 
     private static void weekDay() {
